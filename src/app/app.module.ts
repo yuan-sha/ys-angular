7e82699966aaadf3b9eff9a7a23150a35e173250
Yuan Sha <ysha@progrexion.com>
2020-07-08 14:32:58 -0600

    页面调整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 通知模块
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,10 +16,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // 通知模块
     ToastModule,
     BrowserAnimationsModule
   ],
   providers: [
+    // 通知模块
     MessageService
   ],
   bootstrap: [AppComponent]
