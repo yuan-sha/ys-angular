936253e9bd0e958c0e31405f76db4a4c24dcc8ce
Yuan Sha <ysha@progrexion.com>
2020-07-08 14:13:29 -0600

    update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ys-angular';
+  public showToggleMenu = false;
+
+  onMenuToggle() {
+    this.showToggleMenu = !this.showToggleMenu;
+  }
+
 }
