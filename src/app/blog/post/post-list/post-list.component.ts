4a7c63643cb3922784ba649b07e29442b58229bc
Yuan Sha <ysha@progrexion.com>
2020-07-09 16:48:07 -0600

    backup

--- src/app/blog/post/post-list/post-list.component.ts
+++ src/app/blog/post/post-list/post-list.component.ts
@@ -7,6 +7,11 @@ import { PostService } from '../post.service';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+  public itemsPerPage = 10;
+  public totalRecords = 11;
+  public currentPage = 1;
+  public offset = 0;
+  public end = 0;
   public postList: Array<any>;
 
   constructor(
@@ -25,6 +30,9 @@ export class PostListComponent implements OnInit {
       error => { console.log(error); },
       () => { }
     );
+  }
+
+  public pageChanged(event: any): void {
   }
 
 }
