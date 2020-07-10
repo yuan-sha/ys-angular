323ce5be7224e9094777ca1e3396a430d53635a7
Yuan Sha <ysha@progrexion.com>
2020-07-10 14:19:52 -0600

    分页按钮

--- src/app/blog/post/post.module.ts
+++ src/app/blog/post/post.module.ts
@@ -5,12 +5,14 @@ import { PostRoutingModule } from './post-routing.module';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostHeadlingComponent } from './post-headling/post-headling.component';
 
+import {PaginatorModule} from 'primeng/paginator';
 
 @NgModule({
   declarations: [PostListComponent, PostHeadlingComponent],
   imports: [
     CommonModule,
-    PostRoutingModule
+    PostRoutingModule,
+    PaginatorModule
   ]
 })
 export class PostModule { }
