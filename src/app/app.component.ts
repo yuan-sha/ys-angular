325cb807422c3ce22c875b11fe279953b9c98eab
Yuan Sha <ysha@progrexion.com>
2020-08-10 14:15:49 -0600

    登录跳转问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { TranslateService } from '@ngx-translate/core';
+import { SignInService } from './blog/user/sign-in/sign-in.service';
+import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +17,39 @@ export class AppComponent implements OnInit{
   public currentUser: any;
 
   constructor(
+    public activatedRoute: ActivatedRoute,
+    public router: Router,
     private messageService: MessageService,
-    public translateService: TranslateService
+    public translateService: TranslateService,
+    public signInService: SignInService
   ) {
   }
 
   ngOnInit(){
     const browserLang = this.translateService.getBrowserLang();
     this.changeLanguage(browserLang);
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+
+    merge(this.signInService.currentUser)
+      .subscribe(
+        data => {
+          this.currentUser = data;
+          const activatedRouteSnapshot: ActivatedRouteSnapshot = this.activatedRoute.snapshot;
+          const routerState: RouterState = this.router.routerState;
+          const routerStateSnapshot: RouterStateSnapshot = routerState.snapshot;
+
+          console.log(activatedRouteSnapshot);
+          console.log(routerState);
+          console.log(routerStateSnapshot);
+
+          // 如果是从/login这个URL进行的登录，跳转到首页，否则什么都不做
+          if (routerStateSnapshot.url.indexOf('/login') != -1) {
+            this.router.navigateByUrl('/home');
+          }
+        },
+        error => console.error(error)
+      );
   }
 
   changeLanguage(lang: string){
@@ -48,6 +76,10 @@ export class AppComponent implements OnInit{
   }
 
   public doLogout(): void {
+    this.showToggleMenu = false;
+    this.signInService.logout();
+    this.messageService.add({ severity: 'success', summary: 'Success Message', detail: '退出成功', life: 500 });
+    this.router.navigateByUrl('');
   }
 
 }
--- src/app/blog/user/sign-in/sign-in.component.ts
+++ src/app/blog/user/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeIn } from '../../../shared/animations/fade-in';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
+import { SignInService } from './sign-in.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,12 +13,27 @@ export class SignInComponent implements OnInit {
   public user: any = {};
   public error: Error;
 
-  constructor(public router: Router) { }
+  constructor(public router: Router,
+              public activatedRoute: ActivatedRoute,
+              public signInService: SignInService) { }
 
   ngOnInit(): void {
+    console.log('--- sign-in-component ---');
+    console.log(this.router);
+    console.log(this.activatedRoute);
+
+    const activatedRouteSnapshot: ActivatedRouteSnapshot = this.activatedRoute.snapshot;
+    const routerState: RouterState = this.router.routerState;
+    const routerStateSnapshot: RouterStateSnapshot = routerState.snapshot;
+
+    console.log(activatedRouteSnapshot);
+    console.log(routerState);
+    console.log(routerStateSnapshot);
   }
 
   public doLogin(): void {
+    console.log(this.user);
+    this.signInService.login();
   }
 
   public retrievePwd(): void {
